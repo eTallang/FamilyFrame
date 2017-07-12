13187ac674c114484c5532fff0b09ce1d42fcc3b
Erik Tallang <et@computas.int>
2017-07-13 00:23:38 +0200

    Add toggle-logic for upload-button

--- src/app/ui/upload/upload-card/upload-card.component.ts
+++ src/app/ui/upload/upload-card/upload-card.component.ts
@@ -1,10 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ff-upload-card',
   templateUrl: './upload-card.component.html',
   styleUrls: ['./upload-card.component.scss']
 })
-export class UploadCardComponent {
+export class UploadCardComponent implements OnInit {
   @Input() photos: File[] = [];
+  state: string;
+
+  ngOnInit() {
+    this.state = 'pause';
+  }
+
+  toggleState() {
+    if (this.state === 'pause') {
+      this.state = 'uploading';
+    } else {
+      this.state = 'pause';
+    }
+  }
+
+  isPaused(): boolean {
+    return this.state === 'pause';
+  }
 }
