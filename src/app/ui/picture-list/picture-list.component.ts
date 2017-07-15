f5dae97f3121cdc7ad9e2d03dabce48cd14c1575
Erik Tallang <et@computas.int>
2017-07-15 14:57:40 +0200

    Add staggering animation on picture list

--- src/app/ui/picture-list/picture-list.component.ts
+++ src/app/ui/picture-list/picture-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { trigger, style, transition, animate, query, keyframes, stagger } from '@angular/animations';
 
 import { PictureService } from '../../services';
 import { Picture } from '../../interfaces';
@@ -6,10 +7,23 @@ import { Picture } from '../../interfaces';
 @Component({
   selector: 'ff-picture-list',
   templateUrl: './picture-list.component.html',
-  styleUrls: ['./picture-list.component.scss']
+  styleUrls: ['./picture-list.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+        query(':enter', stagger('40ms', [
+          animate('500ms cubic-bezier(0,.59,.39,1)', keyframes([
+            style({ opacity: 0, transform: 'translateY(-20px)', offset: 0}),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]), { optional: true } )
+      ])
+    ])
+  ]
 })
 export class PictureListComponent implements OnInit {
-  pictures: Picture[];
+  pictures: Picture[] = [];
 
   constructor(private pictureService: PictureService, private changeDetectorRef: ChangeDetectorRef) { }
 
