6cd5d98a8192ca90195f930dd47dc531fe7f8b5d
Erik Tallang <e.tallang@gmail.com>
2017-07-19 13:24:07 +0200

    Polish the upload-animation

--- src/app/ui/upload/upload-panel/upload-panel.component.ts
+++ src/app/ui/upload/upload-panel/upload-panel.component.ts
@@ -13,19 +13,25 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
         animate('400ms cubic-bezier(0.0, 0.0, 0.2, 1)')
       ]),
       transition('* => void', [
-        animate(0, style({ transform: 'translateY(-30px)', opacity: 0 }))
+        animate('400ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({ height: '0px' }))
       ])
     ])
   ]
 })
 export class UploadPanelComponent {
   photos: File[];
+  showButton = true;
 
   setPhotos(photos: File[]) {
     this.photos = photos;
+    this.showButton = false;
   }
 
   clearPhotos() {
     this.photos = null;
+    const that = this;
+    setTimeout(function () {
+      that.showButton = true;
+    }, 800);
   }
 }
